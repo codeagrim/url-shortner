b809df73d906c667872ff6e7a7b9142c8cff0518
codeagrim <agrim32sharma@gmail.com>
2025-06-25 21:49:13 +0530

    Created Auth Model

---
+++ models/auth.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+
+    createdAt: {
+
+        type: Date,
+        default: Date.now,
+    }
+
+},
+    { timestamps: true }
+);
+
+export const usermodel = mongoose.model('User', accountSchema)
