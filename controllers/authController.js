1110fc2c3ed5f8141edb9d41faa71a4a0f81e014
codeagrim <agrim32sharma@gmail.com>
2025-07-03 23:41:25 +0530

    feat(auth): add JWT signing after successful user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 import {usermodel} from "../models/auth.model.js"
+import bcrypt from "bcrypt";
+import { json } from "express";
+import jwt from "jsonwebtoken";
 
 
 async function HandleSignup(req,res)
@@ -30,11 +33,9 @@ async function HandleSignup(req,res)
 async function HandleLogin(req, res)
 
 {
-
-
     const {email , password} = req.body;
 
-    const user = await usermodel.findOne({email, password})
+    const user = await usermodel.findOne({email})
     
     if(!user){
         return res.json({message: "User Not Found !! Sign Up First"})
@@ -42,7 +43,24 @@ async function HandleLogin(req, res)
 
     const isMatch = await bcrypt.compare(password, user.password);
 
+    if(!isMatch){
+        return res.status(401).json({ message: "Invalid credentials" });
+    }
 
+
+    //server checks the credentials if valid server jwt banata hai jaisa niche ho rha hai 
+
+    // Server sends JWT as a JSON response
+
+
+     const token = jwt.sign(
+        { id: user._id }, 
+        process.env.JWT_SECRET, 
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.json({ token });
+    
 }
 
 
